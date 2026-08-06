26340e21e0f2d0c2f42296f3d2a6bf2239a48a86
qkrjsf <junseong9o8@gmail.com>
2026-08-06 14:57:49 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,10 @@
-const CACHE_NAME = 'sulbi-jindan-v11';
+const CACHE_NAME = 'sulbi-jindan-v14';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png',
-  './logo.png'
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
